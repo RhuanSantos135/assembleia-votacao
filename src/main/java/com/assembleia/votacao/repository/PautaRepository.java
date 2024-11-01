59ee2aa0e7edb5676d14691e788a1de2a6abe864
Rhuan Santos | Equals <rhuan.santos@equals.com.br>
2024-11-01 13:13:52 -0300

    Subindo a alteração para a consulta de validação de pauta já existente

--- src/main/java/com/assembleia/votacao/repository/PautaRepository.java
+++ src/main/java/com/assembleia/votacao/repository/PautaRepository.java
@@ -4,5 +4,10 @@ import com.assembleia.votacao.domain.Pauta;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.http.ResponseEntity;
 
+import java.util.Optional;
+
 public interface PautaRepository extends JpaRepository<Pauta, Long> {
+
+
+    Optional<Pauta> findByTitulo(String titulo);
 }
