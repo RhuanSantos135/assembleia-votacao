9bbac6fa17264a6a5aaebb4c0cfa41e7c96f0c4d
RhuanSantos135 <rhuansantos507@gmail.com>
2024-12-17 17:16:21 -0300

    Subindo projeto de autenticação

--- src/main/java/com/assembleia/votacao/repository/UsuarioRepository.java
+++ src/main/java/com/assembleia/votacao/repository/UsuarioRepository.java
@@ -11,6 +11,6 @@ import java.util.Optional;
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
 
-    Optional<Usuario> findByEmail(String email);
+    Usuario findByEmail(String email);
 
 }
